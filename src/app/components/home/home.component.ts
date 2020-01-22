0afb63c2864d2f4e6c82caabf56d2dcadc6ba0ea
patorma <pocontreras@ing.ucsc.cl>
2020-01-22 00:24:33 -0300

    ejemplo de peticion http

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  //Obtener datos especificos de la data de la peticion
+  paises: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+    console.log('Constructor del Home hecho');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe((resp: any) => {
+        //cuando se obtenga la informacion, voy hacer que this.paises sea igual a la respuesta
+        this.paises = resp;
+        console.log(resp);
+      })
+   }
 
   ngOnInit() {
   }
