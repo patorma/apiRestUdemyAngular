b7487e659a7e310a934fce75301f997a3cca9afc
patorma <pocontreras@ing.ucsc.cl>
2020-01-21 23:53:21 -0300

    se crearon componentes, se agrego bootstrap y se hicieron configuraciones iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home'},
+ // { path: '**', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
